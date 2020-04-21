d5bb3aee653ad5c87de3b6ed996cc9bfa77d705e
anupamtripathi <atanupamtripathi19@gmail.com>
2020-04-20 23:58:23 -0500

    Initial Commit

--- Smear_Detection/apply_filters.py
+++ Smear_Detection/apply_filters.py
@@ -2,44 +2,18 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import numpy as np
-import utils
+from utils import apply_laplacian, hist_eq
 
 # from apply_filters import apply_laplacian
 
-
-def apply_laplacian(src):
-    ddepth = cv2.CV_16S
-    # src = cv2.imread(cv2.samples.findFile(r'WhatsApp Image 2020-04-20 at 7.58.55 PM.jpeg'), cv2.IMREAD_COLOR) # Load an image
-    # src = cv2.resize(src, (500, 500))
-    src = cv2.GaussianBlur(src, (3, 3), 0)
-    # print(src)
-    src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-    # print(src_gray)
-    dst = cv2.Laplacian(src_gray, ddepth, ksize=3)
-    print(dst)
-    abs_dst = cv2.convertScaleAbs(dst)
-    # print(abs_dst)
-    return abs_dst
 
 
 img = cv2.imread(r'393408669.jpg')
 print(img.shape)
 
-# img = 255 - img
 
-# kernel = np.ones((5, 5), np.float32)/25
-kernel = np.array([[0, -1, 0],
-                  [-1, 4, -1],
-                  [0, -1, 0]])
-dst = cv2.filter2D(img, -1, kernel)
-median = cv2.medianBlur(img, 25)
-new = img - median
+img2 = hist_eq(img)
 
-img2 = cdf[img]
-
-# print(lap, np.max(lap))
-# lap = (lap / np.max(lap)) * 255
-# print(lap, np.max(lap))
 
 plt.subplot(241), plt.imshow(img), plt.title('Original')
 plt.xticks([]), plt.yticks([])
@@ -66,4 +40,8 @@ plt.xticks([]), plt.yticks([])
 # cv2.waitKey(0)
 
 plt.show()
+
+
+if __name__ == '__main__':
+    img = cv2.imread(r'393408669.jpg')
 
