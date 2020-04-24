5a9f071e60fd3bf8baf135a7864222fcda475cb9
rautnikita <rautnikita77@gmail.com>
2020-04-24 13:35:04 -0500

    Initial Commit

---
+++ Smear_Detection/main2.py
@@ -0,0 +1,50 @@
+import cv2
+import os
+import numpy as np
+from tqdm import tqdm
+import math
+from utils import hist_eq, plot_cam2_bounding_box, gaussian_blur, dilate
+
+
+
+def load_images_from_folder(folder):
+    mean_img = cv2.imread(os.path.join(folder, os.listdir(folder)[0]),0)
+    mean_img_equ = cv2.equalizeHist(mean_img)
+    mean_img_equ = mean_img - mean_img_equ
+    mean_img_gauss = cv2.GaussianBlur(mean_img_equ, (9, 9), 0)
+    for idx, filename in enumerate(tqdm(os.listdir(folder)[1:])):
+        img = cv2.imread(os.path.join(folder, filename),0)
+        if img is not None:
+            img_equ = cv2.equalizeHist(img)
+            img_equ = img - img_equ
+            img_gauss = gaussian_blur(img_equ, 11, iterations=2)
+            mean_img_gauss = (mean_img_gauss * (idx + 1) + img_gauss) / (idx + 2)
+
+    return mean_img_gauss
+
+
+
+def main():
+    # cam = 'cam_2'
+    # mean = load_images_from_folder(os.path.join('data', 'sample_drive', cam))
+    # cv2.imwrite("mean.png", mean)
+    mean = cv2.imread('mean.png')
+
+    kernel = np.ones((3, 3), np.uint8)
+    bg = cv2.dilate(mean, kernel, iterations=1)
+    # cv2.imwrite("bg.png", bg)
+    mask = mean - bg
+    # mask = 255 - mask
+    cv2.imwrite("bg.png", mask)
+
+    for iterations in range(1):
+        mask = cv2.medianBlur(mask, 15)
+        cv2.imwrite(str(iterations)+"bg.png", mask)
+    cv2.imwrite('mask.png', mask)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
