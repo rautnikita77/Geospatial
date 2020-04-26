a39e17faccdc19fbcb48473976c2f791c627f55e
anupamtripathi <atanupamtripathi19@gmail.com>
2020-04-26 00:45:24 -0500

    Initial Commit

--- Smear_Detection/main.py
+++ Smear_Detection/main.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-from utils import plot_bounding_box, erode, dilate, median_blur
+from utils import plot_bounding_box, erode, dilate, median_blur, gaussian_blur
 import os
 from tqdm import tqdm
 
@@ -13,6 +13,7 @@ def detect_smear_camer(camera):
     for n, img in enumerate(tqdm(os.listdir(data_path)[1:])):
         image = cv2.imread(os.path.join(data_path, img), cv2.IMREAD_GRAYSCALE)
         equ = cv2.equalizeHist(image)
+        equ = gaussian_blur(equ, 9)
         mean = (mean * (n + 1) + equ) / (n + 2)
 
     mean = cv2.cvtColor(mean.astype(np.uint8), cv2.COLOR_GRAY2RGB)
@@ -20,11 +21,18 @@ def detect_smear_camer(camera):
     bg = dilate(mean, 3, iterations=1)
     mask = mean - bg
     plot_bounding_box(mask, -1, 'Foreground ' + str(camera))
-    mask = erode(mask, 3, 2)
-    mask = median_blur(mask, 7, 4)
-    mask = dilate(mask, 11, 5)
+    mask = median_blur(mask, 101, 2)
     plot_bounding_box(mask, -1, 'Mask camera ' + str(camera))
+
+    params = cv2.SimpleBlobDetector_Params()
+
+    params.filterByArea = True
+    params.minArea = 300
+    params.maxArea = mask.shape[0] * mask.shape[1] / 2
+
+    mask = 255 - mask
+    detector = cv2.SimpleBlobDetector_create(params)
 
 
 if __name__ == "__main__":
-    detect_smear_camer(2)
+    detect_smear_camer(3)
