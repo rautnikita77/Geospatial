450c5a247474bca930700cc613922987c6375581
anupamtripathi <atanupamtripathi19@gmail.com>
2020-04-26 00:55:13 -0500

    Initial Commit

--- Smear_Detection/main.py
+++ Smear_Detection/main.py
@@ -10,17 +10,20 @@ data_path = 'data/sample_drive/cam_' + str(camera)
 
 def detect_smear_camer(camera):
     mean = cv2.imread(os.path.join(data_path, os.listdir(data_path)[0]), cv2.IMREAD_GRAYSCALE)
+    equ = cv2.equalizeHist(mean)
+    mean = gaussian_blur(mean - equ, 9)
     for n, img in enumerate(tqdm(os.listdir(data_path)[1:])):
         image = cv2.imread(os.path.join(data_path, img), cv2.IMREAD_GRAYSCALE)
         equ = cv2.equalizeHist(image)
+        equ = image - equ
         equ = gaussian_blur(equ, 9)
         mean = (mean * (n + 1) + equ) / (n + 2)
 
     mean = cv2.cvtColor(mean.astype(np.uint8), cv2.COLOR_GRAY2RGB)
-    plot_bounding_box(mean, -1, 'Mean camera ' + str(camera))
+    # plot_bounding_box(mean, -1, 'Mean camera ' + str(camera))
     bg = dilate(mean, 3, iterations=1)
     mask = mean - bg
-    plot_bounding_box(mask, -1, 'Foreground ' + str(camera))
+    # plot_bounding_box(mask, -1, 'Foreground ' + str(camera))
     mask = median_blur(mask, 101, 2)
     plot_bounding_box(mask, -1, 'Mask camera ' + str(camera))
 
