51950cd2ca2543613d29e76ccd2684f6fe85f98e
anupamtripathi <atanupamtripathi19@gmail.com>
2020-05-02 22:28:16 -0500

    Initial Commit

--- Slope_Estimation/map_matching.py
+++ Slope_Estimation/map_matching.py
@@ -1,5 +1,14 @@
 from Slope_Estimation.utils import gps_to_ecef_pyproj
 
 
-def refine_points(candidate_points):
+def refine_points(link_id, candidate_points):
+    """
+    Refines the candidate points
+    Args:
+        link_id (int): Identifier for link
+        candidate_points (ndarray): List of candidate points
+
+    Returns:
+        refined_points (ndarray): List of refined points
+    """
     pass
