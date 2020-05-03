e8858a120d50abf4e5b6c5bcc4b2c57c8449ca0d
rautnikita <rautnikita77@gmail.com>
2020-05-03 02:10:09 -0500

    Initial Commit

--- Slope_Estimation/map_matching.py
+++ Slope_Estimation/map_matching.py
@@ -1,16 +1,67 @@
 import os
+import math
 import pandas as pd
+from Slope_Estimation.utils import gps_to_ecef_pyproj
+from tqdm import tqdm
 
 data = 'probe_data_map_matching'
 
+link_dict, probe_dict = {}, {}
+err = 2
+
+
+def find_candidate_points(link_data, probe_data):
+    candidates = []
+    for index, row in tqdm(link_data.iterrows()):
+        link_dict[index] = {}
+        link_dict[index]['toRefSpeedLimit'] = row.toRefSpeedLimit
+        link_dict[index]['fromRefSpeedLimit'] = row.fromRefSpeedLimit
+        link_dict[index]['toRefNumLanes'] = row.toRefNumLanes
+        link_dict[index]['fromRefNumLanes'] = row.fromRefNumLanes
+        link_dict[index]['Candidates'] = []
+        points = [(x.split('/')) for x in row.shapeInfo.split('|')]
+        # print(points)
+        for i in tqdm(range(len(points)-1)):
+            s = gps_to_ecef_pyproj(list(map(float, points[i][:2])))
+            e = gps_to_ecef_pyproj(list(map(float, points[i + 1][:2])))
+            theta = (s[1] - e[1]) / (s[0] - e[0])
+            (d1, d2) = (int(link_dict[index]['toRefNumLanes'])*2, int(link_dict[index]['fromRefSpeedLimit'])*2)
+            (x1, y1) = (s[0] + d1*math.sin(theta) + err, s[1] + d1*math.cos(theta) + err)
+            (x2, y2) = (e[0] + d2*math.sin(theta) - err, e[1] + d2*math.cos(theta) - err)
+            x1, x2 = min(x1, x2), max(x1, x2)
+            y1, y2 = min(y1, y2), max(y1, y2)
+            link_dict[index]['co-ordinates'] = [s, e]
+            for index1, probe in tqdm(probe_data.iterrows()):
+                probe_dict[index1] = {}
+                probe_dict[index1]['sampleID'] = probe.sampleID
+                (x, y) = gps_to_ecef_pyproj([probe.latitude, probe.longitude])
+                probe_dict[index1]['co-ordinates'] = (x, y)
+                probe_dict[index1]['speed'] = probe.speed
+                probe_dict[index1]['heading'] = probe.heading
+                # print(probe_dict)
+                if (x1 < x < x2) and (y1 < y < y2):
+                    link_dict[index]['Candidates'].append(index1)
+                # print(candidates)
+
+
+    return candidates
+
 def main():
 
-    link_cols = ['']
-    probe_cols = ['sampleID', 'dataTime']
-    link_data = pd.read_csv(os.path.join(data, 'Partition6467LinkData.csv'))
-    probe_data = pd.read_csv(os.path.join(data, 'Partition6467ProbePoints.csv'))
+    link_cols = ['linkPVID', 'fromRefSpeedLimit', 'toRefSpeedLimit', 'fromRefNumLanes', 'toRefNumLanes', 'shapeInfo']
+    probe_cols = ['sampleID', 'latitude', 'longitude', 'altitude', 'speed', 'heading']
+    link_header = ['linkPVID', 'refNodeID', 'nrefNodeID', 'length', 'functionalClass', 'directionOfTravel', 'speedCategory',
+                   'fromRefSpeedLimit', 'toRefSpeedLimit', 'fromRefNumLanes', 'toRefNumLanes', 'multiDigitized', 'urban',
+                   'timeZone', 'shapeInfo', 'curvatureInfo', 'slopeInfo']
+    probe_header = ['sampleID', 'dateTime', 'sourceCode', 'latitude', 'longitude', 'altitude', 'speed', 'heading']
+    link_data = pd.read_csv(os.path.join(data, 'Partition6467LinkData.csv'), names=link_header, usecols=link_cols,
+                            index_col='linkPVID')
+    probe_data = pd.read_csv(os.path.join(data, 'Partition6467ProbePoints.csv'), names=probe_header, usecols=probe_cols)
+
+    candidates = find_candidate_points(link_data, probe_data)
+    print(link_dict)
 
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
--- Slope_Estimation/utils.py
+++ Slope_Estimation/utils.py
@@ -12,9 +12,21 @@ def plot_lat_long_points(points):
     plt.show()
 
 
-def gps_to_ecef_pyproj(lat, lon, alt):
-    ecef = pyproj.Proj(proj='geocent', ellps='WGS84', datum='WGS84')
-    lla = pyproj.Proj(proj='latlong', ellps='WGS84', datum='WGS84')
-    x, y, z = pyproj.transform(lla, ecef, lon, lat, alt, radians=False)
+def gps_to_ecef_pyproj(lat_long_alt):
 
-    return x, y, z
+    if len(lat_long_alt) == 3:
+        ecef = pyproj.Proj(proj='geocent', ellps='WGS84', datum='WGS84')
+        lla = pyproj.Proj(proj='latlong', ellps='WGS84', datum='WGS84')
+        x, y, z = pyproj.transform(lla, ecef, lat_long_alt[0], lat_long_alt[1], lat_long_alt[2], radians=False)
+        return (x, y, z)
+    else:
+        ecef = pyproj.Proj(proj='geocent', ellps='WGS84', datum='WGS84')
+        lla = pyproj.Proj(proj='latlong', ellps='WGS84', datum='WGS84')
+        x, y = pyproj.transform(lla, ecef, lat_long_alt[0], lat_long_alt[1], radians=False)
+        return (x, y)
+
+
+
+
+
+print(gps_to_ecef_pyproj([2,2]))
