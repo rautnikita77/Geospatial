02d9cd4a4db22009c3bfc5d4cac6b89c469431a7
anupamtripathi <atanupamtripathi19@gmail.com>
2020-05-02 22:10:59 -0500

    Initial Commit

--- Slope_Estimation/utils.py
+++ Slope_Estimation/utils.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import pyproj
 
 
 def plot_lat_long_points(points):
@@ -9,3 +10,11 @@ def plot_lat_long_points(points):
     plt.scatter(x=[x[0] for x in points], y=[x[1] for x in points])
     plt.plot(points[0], points[-1])
     plt.show()
+
+
+def gps_to_ecef_pyproj(lat, lon, alt):
+    ecef = pyproj.Proj(proj='geocent', ellps='WGS84', datum='WGS84')
+    lla = pyproj.Proj(proj='latlong', ellps='WGS84', datum='WGS84')
+    x, y, z = pyproj.transform(lla, ecef, lon, lat, alt, radians=False)
+
+    return x, y, z
