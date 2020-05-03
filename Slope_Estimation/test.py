1a230d9a7993457f1d307517a09250e18cb793f9
rautnikita <rautnikita77@gmail.com>
2020-05-03 05:38:31 -0500

    Initial Commit

--- Slope_Estimation/test.py
+++ Slope_Estimation/test.py
@@ -1,23 +1,6 @@
-import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
+import numpy as np
 
-arr_test = [[1, 2, 1], [1, 2, 2], [1, 2, 2]]
-arr_train = [[1, 2, 1], [1, 2, 2], [1, 2, 3]]
-df_train = pd.DataFrame(data=arr_train, columns=['a', 'b', 'c'])
-df_test = pd.DataFrame(data=arr_test, columns=['a', 'b', 'c'])
-print(df_test)
-enc = OneHotEncoder(handle_unknown='ignore')
-enc.fit(pd.DataFrame(df_train['c']))
-one_hot = enc.transform(pd.DataFrame(df_train['c'])).toarray()
-one_hot = pd.DataFrame(data=one_hot, columns=['1', '2', '3'])
-print(one_hot)
-df_train = pd.concat((df_train, one_hot), axis=1, sort=False)
-print(df_train)
-
-one_hot = enc.transform(pd.DataFrame(df_test['c'])).toarray()
-one_hot = pd.DataFrame(data=one_hot, columns=['1', '2', '3'])
-print(one_hot)
-df_test = pd.concat((df_test, one_hot), axis=1, sort=False)
-print(df_test)
+arr = np.array([1, 2, 3, 7, 10, 20, 21, 22, 23, 50])
+print(np.quantile(arr, 0.75))
 
 
