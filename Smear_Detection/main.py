bf4c9f3751200eca85e33f2a0a8614ab0a1c4ae9
rautnikita <rautnikita77@gmail.com>
2020-05-02 19:14:01 -0500

    Initial Commit

--- Smear_Detection/main.py
+++ Smear_Detection/main.py
@@ -4,27 +4,32 @@ from utils import plot_bounding_box, erode, dilate, median_blur, gaussian_blur
 import os
 from tqdm import tqdm
 
-camera = 2
+camera = 3
 data_path = 'data/sample_drive/cam_' + str(camera)
 
 
 def detect_smear_camer(camera):
-    mean = cv2.imread(os.path.join(data_path, os.listdir(data_path)[0]), cv2.IMREAD_GRAYSCALE)
+    mean = cv2.imread(os.path.join(data_path, os.listdir(data_path)[0]), 0)
     equ = cv2.equalizeHist(mean)
-    mean = gaussian_blur(mean - equ, 9)
+    mean = cv2.GaussianBlur(equ, (9, 9), 0)
     for n, img in enumerate(tqdm(os.listdir(data_path)[1:])):
         image = cv2.imread(os.path.join(data_path, img), cv2.IMREAD_GRAYSCALE)
         equ = cv2.equalizeHist(image)
-        equ = image - equ
-        equ = gaussian_blur(equ, 9)
+        equ = cv2.GaussianBlur(equ, (9, 9), 0)
         mean = (mean * (n + 1) + equ) / (n + 2)
 
-    mean = cv2.cvtColor(mean.astype(np.uint8), cv2.COLOR_GRAY2RGB)
+    cv2.imwrite("mean.png", mean)
+    mean = cv2.imread('mean.png')
     # plot_bounding_box(mean, -1, 'Mean camera ' + str(camera))
-    bg = dilate(mean, 3, iterations=1)
+    kernel = np.ones((3, 3), np.uint8)
+    bg = cv2.dilate(mean, kernel, iterations=1)
     mask = mean - bg
+    cv2.imwrite("bg2.png", mask)
     # plot_bounding_box(mask, -1, 'Foreground ' + str(camera))
-    mask = median_blur(mask, 101, 2)
+    for iterations in range(2):
+        mask = cv2.medianBlur(mask, 101)
+
+    cv2.imwrite("a.png", mask)
     plot_bounding_box(mask, -1, 'Mask camera ' + str(camera))
 
     params = cv2.SimpleBlobDetector_Params()
