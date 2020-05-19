78c24e441571a4fd064a0ae87b33c0ddc566193c
rautnikita <rautnikita77@gmail.com>
2020-05-19 05:06:02 -0500

    Initial Commit

--- Aerial_Imagery/bing_maps.py
+++ Aerial_Imagery/bing_maps.py
@@ -1,13 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib import request
+from PIL import Image
+
 def get_bing_map(min_lat, max_lat, min_lon, max_lon):
     """
     Returns the aerial image for the bounding box defined
     Args:
-        min_lat (int): Minimum latitude
-        max_lat (int): Max latitude
-        min_lon (int): Minimum long
-        max_lon (int): Max long
+        min_lat (float): Minimum latitude
+        max_lat (float): Max latitude
+        min_lon (float): Minimum long
+        max_lon (float): Max long
 
     Returns:
 
     """
-    pass
+    key = "AgVkIXYlkkwD-wKoY9IQDYy1bvLHiAaWC2ZI_LewXAfPxf-IYQeE_PLu3A9Z4ZxA"
+    BASEURL = "https://dev.virtualearth.net/REST/V1/Imagery/Map/Aerial/?mapArea={},{},{},{}&key={}"\
+        .format(str(min_lat), str(max_lat), str(min_lon), str(max_lon), key)
+
+    with request.urlopen(BASEURL) as file:
+        a = Image.open(file)
+    a.show()
+    return a
+
+
+get_bing_map(37.317227, -122.318439, 37.939081, -122.194565)
+
+
